1b1dd820890231131bc2dca530755d5191c4e3a2
Neriton Tolentino <neriton.tolentino@nubank.com.br>
2023-05-19 19:30:53 -0300

    feat: Add Show model and set relationships

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ class Venue(db.Model):
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(120))
+    shows = db.relationship('Show', backref='venue', lazy=True)
 
 
 class Artist(db.Model):
@@ -59,9 +60,17 @@ class Artist(db.Model):
     website = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(120))
-    
+    shows = db.relationship('Show', backref='artist', lazy=True)
 
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+class Show(db.Model):
+   __tablename__ = 'Show'
+
+   id = db.Column(db.Integer, primary_key=True)
+   venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+   artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+   start_time = db.Column(db.DateTime, nullable=False)
+   
 
 #----------------------------------------------------------------------------#
 # Filters.
