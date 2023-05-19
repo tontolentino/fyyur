5816cc1eb643e4e012ee758fcc0a1a9c511dadbb
Neriton Tolentino <neriton.tolentino@nubank.com.br>
2023-05-19 19:47:09 -0300

    refact: Add genres attribute to Venue model

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ class Venue(db.Model):
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String(120)))
     shows = db.relationship('Show', backref='venue', lazy=True)
 
 
@@ -70,7 +71,7 @@ class Show(db.Model):
    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
    start_time = db.Column(db.DateTime, nullable=False)
-   
+
 
 #----------------------------------------------------------------------------#
 # Filters.
