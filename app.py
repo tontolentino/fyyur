f9a56b89fb625cc4a864221ba2d774d66a788532
Neriton Tolentino <neriton.tolentino@nubank.com.br>
2023-05-24 21:43:54 -0300

    feat: Implement search_artists controller

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def venues():
 def search_venues():
 
   search_term = request.form.get('search_term', '')
-  venues = Venue.query.filter(Venue.name.ilike(f'%{search_term}%'))
+  venues = Venue.query.filter(Venue.name.ilike(f'%{search_term}%')).all()
 
   data = []
 
@@ -265,32 +265,41 @@ def artists():
     for row in artists
   ]
 
-  # TODO: replace with real data returned from querying the database
-  # data=[{
-  #   "id": 4,
-  #   "name": "Guns N Petals",
-  # }, {
-  #   "id": 5,
-  #   "name": "Matt Quevedo",
-  # }, {
-  #   "id": 6,
-  #   "name": "The Wild Sax Band",
-  # }]
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
+
+  search_term = request.form.get('search_term', '')
+  artists = Artist.query.filter(Artist.name.ilike(f'%{search_term}%')).all()
+
+  data = []
+
+  for artist in artists:
+    data.append(
+      {
+        'id': artist.id,
+        'name': artist.name,
+        'num_upcoming_shows': len(Show.query.filter(Show.artist_id == artist.id).all())
+      }
+    )
+
+  response = {
+    'count': len(data),
+    'data': data
+  }
+
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
+  # response={
+  #   "count": 1,
+  #   "data": [{
+  #     "id": 4,
+  #     "name": "Guns N Petals",
+  #     "num_upcoming_shows": 0,
+  #   }]
+  # }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
