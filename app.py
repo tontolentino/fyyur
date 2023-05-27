38dbee4dbe7d2f03a42cbaf9ec2f9aecc6ccfbbe
Neriton Tolentino <neriton.tolentino@nubank.com.br>
2023-05-27 19:42:09 -0300

    feat: Implement create_show_submission controller

--- app.py
+++ app.py
@@ -442,9 +442,6 @@ def create_artist_form():
 
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
-  # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
 
   try:
     artist = Artist(
@@ -504,14 +501,25 @@ def create_shows():
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
-  # called to create new shows in the db, upon submitting new show listing form
-  # TODO: insert form data as a new Show record in the db, instead
+  
+  try:
+    show = Show(
+      artist_id = request.form['artist_id'],
+      venue_id = request.form['venue_id'],
+      start_time = request.form['start_time']
+    )
 
-  # on successful db insert, flash success
-  flash('Show was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Show could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+    db.session.add(show)
+    db.session.commit()
+    flash('Show was successfully listed!')
+
+  except:
+    db.session.rollback()
+    flash('An error occurred. Show could not be listed.')
+  
+  finally:
+    db.session.close()
+
   return render_template('pages/home.html')
 
 @app.errorhandler(404)
