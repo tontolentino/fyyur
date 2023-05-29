a6171b2c9dc31f52c0a20583036838abb788a18d
Neriton Tolentino <neriton.tolentino@nubank.com.br>
2023-05-29 11:13:12 -0300

    refact: Use env variables to connect to db

--- config.py
+++ config.py
@@ -7,10 +7,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+DB_NAME = os.getenv('DB_NAME', 'fyyur')
 
 # IMPLEMENT DATABASE URL 
-SQLALCHEMY_DATABASE_URI = 'postgresql://neriton.tolentino@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 
 # To supress deprecation warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
